ada725ca5b7c7d73b683e3a6927af848f48b9406
Kenneth Ceglia <KPZNet@outlook.com>
2021-08-09 07:45:45 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+# A PARTIAL Binary Search Tree
+class Partial_Retroactive_TreeNode:
+
+    # Constructor to create a new node
+    def __init__(self, key):
+        pass
+
+    # Insert to add a new node at a specified time
+    # optionally, may return the time to execute
+    def Insert(node, key, time):
+        pass
+
+    # Insert to delete a new node at a specified time
+    # optionally, may return the time to execute
+    def Delete(root, key, time):
+        pass
+
+    # Query for a value <= X on the current search tree
+    # The return value is X
+    # optionally, may return the time to execute as a second return tuple
+    def Pred(x)->int:
+        pass
+
+    # A utility function to do inorder traversal of BST
+    # This will let us print out the entire tree, will be helpful for debugging
+    def inorder(root):
+        pass
+
+# A FULL retroactive Binary Search Tree
+class Full_Retroactive_TreeNode:
+
+    # Constructor to create a new node
+    # Constructor to create a new node
+    def __init__(self, key):
+        pass
+
+    # Insert to add a new node at a specified time
+    # optionally, may return the time to execute
+    def Insert(node, key, time):
+        pass
+
+    # Insert to delete a new node at a specified time
+    # optionally, may return the time to execute
+    def Delete(root, key, time):
+        pass
+
+    # Query for a value <= X on the current search tree at a given time
+    # The return value is X
+    # optionally, may return the time to execute as a second return tuple
+    def Pred(x, time)->int:
+        pass
+
+    # A utility function to do inorder traversal of BST
+    # This will let us print out the entire tree, will be helpful for debugging
+    def inorder(root):
+        pass
+
+
