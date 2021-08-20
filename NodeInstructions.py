bab54193b66ae7ed486407bf7482c69d4e59b2ca
KPZNET <kpznet@outlook.com>
2021-08-20 16:46:18 -0700

    McDev

--- NodeInstructions.py
+++ NodeInstructions.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from BSTree import BSTree
 
 
@@ -8,10 +7,10 @@ class Instruction :
         self.key = key
         self.payload = payload
         if payload == None :
-            self.payload = str(key)
+            self.payload = str ( key )
 
-    def __str__(self):
-        s = self.instructionCode + " " + str(self.payload) + " key: " + str(self.key)
+    def __str__(self) :
+        s = self.instructionCode + " " + str ( self.payload ) + " key: " + str ( self.key )
         return s
 
 
@@ -27,10 +26,10 @@ class TimeSlot_Instructions :
 
 class TimeLine :
     def __init__(self) :
-        self.BST_TimeSlots = BSTree()
+        self.BST_TimeSlots = BSTree ()
 
-    def gettimeline(self):
-        return self.BST_TimeSlots.inorder()
+    def gettimeline(self) :
+        return self.BST_TimeSlots.inorder ()
 
     def Replay_TimeSlot_Instructions(self, bst, instructions) :
         for inst in instructions :
@@ -43,16 +42,16 @@ class TimeLine :
     def __sf(self, e) :
         return e.time
 
-    def get_time_slots_up_to_time(self, time):
-        return self.BST_TimeSlots.inorderLessThanEqual(time)
+    def get_time_slots_up_to_time(self, time) :
+        return self.BST_TimeSlots.inorderLessThanEqual ( time )
 
-    def get_time_slots_after_time(self, time):
-        return self.BST_TimeSlots.inorderGreaterThan(time)
+    def get_time_slots_after_time(self, time) :
+        return self.BST_TimeSlots.inorderGreaterThan ( time )
 
     def build_tree_up_to_time(self, time, balance="True") :
         tbst = BSTree ()
-        time_slots = self.get_time_slots_up_to_time(time)
-        for time in time_slots:
+        time_slots = self.get_time_slots_up_to_time ( time )
+        for time in time_slots :
             self.Replay_TimeSlot_Instructions ( tbst, time.instructions )
         if balance :
             tbst.rebalance ()
@@ -60,31 +59,29 @@ class TimeLine :
 
     def build_tree_after_time(self, time, balance="True") :
         tbst = BSTree ()
-        time_slots = self.BST_TimeSlots.inorderGreaterThan(time)
-        for time in time_slots:
+        time_slots = self.BST_TimeSlots.inorderGreaterThan ( time )
+        for time in time_slots :
             self.Replay_TimeSlot_Instructions ( tbst, time.instructions )
         if balance :
             tbst.rebalance ()
         return tbst
 
-    def build_all_trees_after_time(self, time, balance="True"):
-        timeSlots = self.get_time_slots_after_time(time)
-        for timeSlot in timeSlots:
-            timeSlot.bst = self.build_tree_up_to_time(timeSlot.time)
+    def build_all_trees_after_time(self, time, balance="True") :
+        timeSlots = self.get_time_slots_after_time ( time )
+        for timeSlot in timeSlots :
+            timeSlot.bst = self.build_tree_up_to_time ( timeSlot.time )
 
+    def Add_TimeSlot(self, timeSlot) :
+        self.BST_TimeSlots.insert ( timeSlot.time, payload=timeSlot )
+        self.BST_TimeSlots.rebalance ()
 
-    def Add_TimeSlot(self, timeSlot):
-        self.BST_TimeSlots.insert(timeSlot.time, payload=timeSlot)
-        self.BST_TimeSlots.rebalance()
-
-        timeSlot.bst = self.build_tree_up_to_time (timeSlot.time, balance=True)
-        self.build_all_trees_after_time(timeSlot.time)
-
+        timeSlot.bst = self.build_tree_up_to_time ( timeSlot.time, balance=True )
+        self.build_all_trees_after_time ( timeSlot.time )
 
     def build_complete_tree(self, keeptree=False, balance="False") :
-        #Inorder BST builder
-        bst = BSTree()
-        nds = self.BST_TimeSlots.inorder()
+        # Inorder BST builder
+        bst = BSTree ()
+        nds = self.BST_TimeSlots.inorder ()
         for inst in nds :
             self.Replay_TimeSlot_Instructions ( bst, inst.payload.instructions )
         if balance :
@@ -94,10 +91,9 @@ class TimeLine :
 
         return bst
 
-
-    def printTimeLine(self):
-        nds = self.gettimeline()
-        for ts in nds:
-            print("Time: {0}".format(ts.payload.time))
-            for inst in ts.payload.instructions:
-                print("\t" + str(inst) )
+    def printTimeLine(self) :
+        nds = self.gettimeline ()
+        for ts in nds :
+            print ( "Time: {0}".format ( ts.payload.time ) )
+            for inst in ts.payload.instructions :
+                print ( "\t" + str ( inst ) )
