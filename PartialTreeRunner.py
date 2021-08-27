2b0767057405caf8d56cf297833698d4021734b8
KPZNET <kpznet@outlook.com>
2021-08-27 07:57:12 -0700

    Testing

--- PartialTreeRunner.py
+++ PartialTreeRunner.py
@@ -12,26 +12,23 @@ class PartialTreeRunner(TreeRunner):
 
     def Run1(self):
         time_slots = 50
-        updates_per_time = 10
-        retro_start = 1
-        retro_end = 1
+        updates_per_time = 5
+        retro_start = 48
+        retro_end = 49
+        averages = 20
 
-        tl = PartialRetroTreeRollback()
-        tm = self.base_run1(tl, time_slots, updates_per_time, retro_start, retro_end)
-        print("Total Time Partial Rollback:{0}".format(tm)) 
-                
-        tl = PartialRetroTree()
-        tm = self.base_run1(tl, time_slots, updates_per_time, retro_start, retro_end)
-        print("Total Time Partial:{0}".format(tm))        
-        
-        tl = PartialRetroTreeRollback()
-        tm = self.base_run1(tl, time_slots, updates_per_time, retro_start, retro_end)
-        print("Total Time Partial Rollback:{0}".format(tm))   
+        tm = 0.0
+        for i in list( range(averages)):
+            tl = PartialRetroTree()
+            tm += self.base_run1(tl, time_slots, updates_per_time, retro_start, retro_end)
+        tm = tm/averages
+        print("Total Time Standard: {0}".format(tm))
 
-        tl = PartialRetroTreeRollback()
-        tm = self.base_run1(tl, time_slots, updates_per_time, retro_start, retro_end)
-        print("Total Time Partial Rollback:{0}".format(tm)) 
-        
-        tl = PartialRetroTree()
-        tm = self.base_run1(tl, time_slots, updates_per_time, retro_start, retro_end)
-        print("Total Time Partial:{0}".format(tm))         
+        tm = 0.0
+        for i in list( range(averages)):
+            tl = PartialRetroTreeRollback()
+            tm += self.base_run1(tl, time_slots, updates_per_time, retro_start, retro_end)
+        tm = tm/averages
+        print("Total Time Rollback: {0}".format(tm))
+
+
