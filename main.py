350697cb42bbbaea02779a122daebfa17d84b37f
Kenneth Ceglia <KPZNet@outlook.com>
2021-08-27 20:53:39 -0700

    main testing

--- main.py
+++ main.py
@@ -13,7 +13,61 @@ sys.setrecursionlimit(1000000)
 print("New Recursion Limit:")
 print(sys.getrecursionlimit())
 
-partial_tree_runner = PartialTreeRunner()
-partial_tree_runner.Run1()
+def tree_run_1(tl):
+
+    il = TimeSlot_Instructions(5)
+    il.addInstruction(Instruction("add", 40))
+    il.addInstruction(Instruction("add", 150))
+    il.addInstruction(Instruction("add", 160))
+    tl.update_tree(il)
+
+    il = TimeSlot_Instructions(10)
+    il.addInstruction(Instruction("add", 39))
+    il.addInstruction(Instruction("add", 20))
+    il.addInstruction(Instruction("add", 70))
+    tl.update_tree(il)
+
+    il = TimeSlot_Instructions(10)
+    il.addInstruction(Instruction("add", 67))
+    il.addInstruction(Instruction("add", 26))
+    il.addInstruction(Instruction("add", 33))
+    tl.update_tree(il)
+
+    il = TimeSlot_Instructions(20)
+    il.addInstruction(Instruction("add", 342))
+    il.addInstruction(Instruction("add", 564))
+    il.addInstruction(Instruction("add", 123))
+    il.addInstruction(Instruction("add", 590))
+    il.addInstruction(Instruction("add", 112))
+    il.addInstruction(Instruction("add", 443))
+    tl.update_tree(il)
+
+    il = TimeSlot_Instructions(5)
+    il.addInstruction(Instruction("del", 40))
+    il.addInstruction(Instruction("del", 150))
+    il.addInstruction(Instruction("del", 160))
+
+    il = TimeSlot_Instructions(23)
+    il.addInstruction(Instruction("del", 342))
+    il.addInstruction(Instruction("del", 564))
+    il.addInstruction(Instruction("del", 123))
+    il.addInstruction(Instruction("del", 590))
+    il.addInstruction(Instruction("del", 112))
+    il.addInstruction(Instruction("del", 443))
+
+    tl.update_tree(il)
+
+    print("------------FINAL------------\n")
+    latest = tl.get_latest_tree()
+    latest.print_tree("FINAL TREE")
+
+
+
+tlf = FullRetroTree ()
+tlp = PartialRetroTree()
+
+tree_run_1(tlf)
+tree_run_1(tlp)
+
 
 
