6c2f6281f7016f3e09d5973d72545ad2427d1f4f
KPZNET <kpznet@outlook.com>
2021-08-30 17:49:01 -0700

    Tests

--- test_RetroBSTreesFull.py
+++ test_RetroBSTreesFull.py
@@ -1,12 +1,31 @@
 from unittest import TestCase
-
+from RetroBSTrees import *
 
 class TestFullRetroTree ( TestCase ) :
     def test_insert(self) :
-        self.fail ()
+        frt = FullRetroTree()
+        frt.Insert(1,1)
+        frt.Insert(2,1)
+        inorder = frt.get_latest_tree().inorder()
+        assert( inorder[0] == '1' and inorder[1] == '2' )
+
+    def test_insert_over_time(self) :
+        frt = FullRetroTree()
+        frt.Insert(1, 100)
+        frt.Insert(2, 200)
+        frt.Insert ( 3, 300 )
+        inorder = frt.get_latest_tree().inorder()
+        assert( inorder[0] == '1' and inorder[1] == '2' and inorder[2] == '3')
 
     def test_delete(self) :
-        self.fail ()
+        frt = FullRetroTree()
+        frt.Insert(1, 100)
+        frt.Insert(2, 100)
+        frt.Insert ( 3, 100 )
+        frt.Delete(2, 100 )
+        frt.Delete ( 3, 100 )
+        inorder = frt.get_latest_tree().inorder()
+        assert( inorder[0] == '1' )
 
     def test_get_latest_tree(self) :
         self.fail ()
